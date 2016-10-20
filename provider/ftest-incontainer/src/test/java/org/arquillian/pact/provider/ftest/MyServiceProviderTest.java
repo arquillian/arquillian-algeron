c60033c10492218b5d07fe1e49767058ec3a3fce
Alex Soto <asotobu@gmail.com>
2016-10-20 18:22:11 +0200

    Revert "[issue #25] add support for provider states with parameters" (#27)

--- provider/ftest-incontainer/src/test/java/org/arquillian/pact/provider/ftest/MyServiceProviderTest.java
+++ provider/ftest-incontainer/src/test/java/org/arquillian/pact/provider/ftest/MyServiceProviderTest.java
@@ -3,8 +3,6 @@ package org.arquillian.pact.provider.ftest;
 import org.arquillian.pact.provider.core.httptarget.Target;
 import org.arquillian.pact.provider.core.loader.PactFolder;
 import org.arquillian.pact.provider.spi.Provider;
-import org.arquillian.pact.provider.spi.State;
-import org.assertj.core.api.Assertions;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -14,9 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.net.URL;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(Arquillian.class)
 @Provider("test_provider")
@@ -33,11 +28,6 @@ public class MyServiceProviderTest {
 
     @ArquillianResource
     Target target;
-
-    @State("test state")
-    public void testStateMethod(Map<String, Object> params) {
-        assertThat(params).containsEntry("name", "Alexandra");
-    }
 
     @Test
     public void should_provide_valid_answers() {
